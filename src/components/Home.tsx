d8cbc5c3f69c600f1ebfaeb2520c69c8ed4f6f38
KIEU TRUNG KIEN <kieutrungkien1382006@gmail.com>
2025-10-15 11:43:40 +0700

    Create Home.tsx

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h2> Chào mừng bạn đến với Bài thực hành Số 02 </h2>
+      <p>Hãy chọn một bài ở menu phía trên để bắt đầu.</p>
+    </div>
+  );
+};
+
+export default Home;
